3010e1040cbb79aa72178e7c21aa183423e79d18
Kam Hung Ho <kevinho0236@gmail.com>
2021-09-20 11:53:46 +0900

    Initial Commit

---
+++ GithubSearchApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GithubSearchApp
+//
+//  Created by Kam Hung Ho on 20/9/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
